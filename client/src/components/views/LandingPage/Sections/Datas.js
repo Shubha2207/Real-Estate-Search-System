7cd9eb69314c77a1d53feba66b8c33871daa2d2d
Shubham Pawar <shubha.pawar2207@gmail.com>
2021-03-23 13:38:13 +0530

    First commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,92 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $10K",
+        "array": [0, 10000]
+    },
+    {
+        "_id": 2,
+        "name": "$10K to $30K",
+        "array": [10000, 30000]
+    },
+    {
+        "_id": 3,
+        "name": "$30K to $60K",
+        "array": [30000, 60000]
+    },
+    {
+        "_id": 4,
+        "name": "More than $60K",
+        "array": [60000, 2000000000]
+    }  
+]
+
+
+
+const cities = [
+    {
+        "_id": 1,
+        "name": "Mumbai"
+    },
+    {
+        "_id": 2,
+        "name": "Thane"
+    },
+    {
+        "_id": 3,
+        "name": "Pune"
+    },
+    {
+        "_id": 4,
+        "name": "Nashik"
+    }
+]
+
+const apartments = [
+    {
+        "_id": 1,
+        "name": "1BHK"
+    },
+    {
+        "_id": 2,
+        "name": "2BHK"
+    },
+    {
+        "_id": 3,
+        "name": "3BHK"
+    },
+    {
+        "_id": 4,
+        "name": "1RK"
+    },
+    {
+        "_id": 5,
+        "name": "House"
+    }
+]
+
+const categories = [
+    {
+        "_id": 1,
+        "name": "Sell"
+    },
+    {
+        "_id": 2,
+        "name": "Rent"
+    }
+]
+
+
+
+export {
+    price,
+    cities,
+    apartments,
+    categories
+}
